aeb45fb5ff7b5cbda139ea1ed427905a0ea9e122
Jamie Cho <jamie@ginkgobioworks.com>
2016-05-21 10:44:05 -0400

    Minor optimizations

--- Primes/primes.c
+++ Primes/primes.c
@@ -1,15 +1,25 @@
+#if 1
 // Based on Basic09 code by James Jones on Facebook
 #include <cmoc.h>
 #include <coco.h>
 typedef byte bool;
+#else
+#include <stdio.h>
+typedef unsigned short uint16_t;
+#define TRUE 1
+#define FALSE 0
+uint16_t getTimer() { return 0; }
+void setTimer(uint16_t t) { }
+typedef unsigned char bool;
+#endif
 
 #define NUM_PRIMES 4000
 uint16_t primes[NUM_PRIMES];
 
 
-bool primetest(uint16_t *primes, uint16_t nprimes, uint16_t candidate) {
-  for(uint16_t ii=0; ii<nprimes; ++ii) {
-    uint16_t prime = primes[ii];
+bool primetest(uint16_t *primes, uint16_t *endprime, uint16_t candidate) {
+  for(uint16_t *ii=primes; ii<endprime; ++ii) {
+    uint16_t prime = *ii;
     uint16_t quot = candidate / prime;
     if (quot < prime)
       return TRUE; 
@@ -21,22 +31,29 @@ bool primetest(uint16_t *primes, uint16_t nprimes, uint16_t candidate) {
 
 
 void primecalc(uint16_t *primes, uint16_t num) {
-  primes[0] = 2;
+  if (num == 0) {
+    return;
+  }
+
+  *primes = 2;
+  uint16_t *maxprime = primes + num;
+  uint16_t *current_prime = primes + 1;
   uint16_t candidate = 3;
   uint16_t nprimes = 1;
 
-  while(nprimes < num) {
-    if (primetest(primes, nprimes, candidate)) {
-      primes[nprimes] = candidate;
+  while(current_prime < maxprime) {
+    if (primetest(primes, current_prime, candidate)) {
+      *current_prime = candidate;
+      ++current_prime;
       ++nprimes;
       if ((nprimes & 3) == 0) {
         printf("%u     %u     %u     %u     %u\n",
-               primes[nprimes-4], primes[nprimes-3],
-               primes[nprimes-2], primes[nprimes-1],
+               *(current_prime - 4), *(current_prime - 3),
+               *(current_prime - 2), *(current_prime - 1),
                getTimer());
       }
     }
-    candidate = candidate + 1;
+    candidate = candidate + 2;
   }
 }
 
