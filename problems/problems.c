c1277e24a1bf5f8641ba1e028163da151de4121d
Jamie Cho <jamie@ginkgobioworks.com>
2016-06-06 18:32:11 -0400

    Add code to illustrate compiler optimization bugs

---
+++ problems/problems.c
@@ -0,0 +1,56 @@
+#include <coco.h>
+
+struct Vect {
+  int x;
+};
+
+struct BigVect {
+  struct Vect x;
+};
+
+void test1() {
+  int vals[10];
+  int ii;
+  unsigned char bvals[10];
+  for(ii=0; ii<sizeof(vals)/sizeof(vals[0]); ii++) {
+    vals[ii] = 0;
+    bvals[ii] = 0;
+  }  
+  vals[5] = 1;
+
+  for(ii=0; ii<sizeof(vals)/sizeof(vals[0]); ii++) {
+    if (vals[ii] == 1)
+      break;
+  }
+  bvals[ii] = (unsigned char)ii;
+  if (bvals[ii] != ii) {
+    printf("test1 failed!\n");
+  }
+}
+
+void test2() {
+  int vals[10];
+  for(int ii=0; ii<sizeof(vals)/sizeof(vals[0]); ii++) {
+    vals[ii] = (ii == 2 || ii == 5) ? 10 : ii;
+  }  
+
+  if (vals[2] != 10 || vals[5] != 10) {
+    printf("test2 failed!\n");
+  }
+}
+
+void crash_cmoc() {
+  struct BigVect xx;
+  if (!xx.x) {
+  }
+}
+
+
+int main() {
+  test1();
+  test2();
+#if 0
+  crash_cmoc();
+#endif
+  return 0;
+}
